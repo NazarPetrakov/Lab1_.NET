c6c547a563b3fae8ddcd1cc5e3df752ddad4a533
NazarPetrakov <nazarpetrakov1@gmail.com>
2023-09-24 14:38:19 +0300

    add IDictionary<> implement with initial fields and ctors

---
+++ MyDictionary/Entries.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class Entries<TKey, TValue>
+    {
+        public TKey key;
+        public TValue value;
+        public int next;
+        public uint hashCode;
+    }
+}
