8e25265597375b5b5029b240ea11a7659bdde106
NazarPetrakov <nazarpetrakov1@gmail.com>
2023-10-20 20:02:43 +0300

    add contains tests

---
+++ MyCollection.Tests/MyCollectionContainsTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCollection.Tests
+{
+    public class MyCollectionContainsTests
+    {
+        [Fact]
+        public void ContainsKeyValue_ExistingPair_ReturnsTrue()
+        {
+            var myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "One");
+
+            bool result = myDictionary.Contains(new KeyValuePair<int, string>(1, "One"));
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ContainsKeyValue_NotExistPair_ReturnsFalse()
+        {
+            var myDictionary = new MyDictionary<int, string>();
+
+            bool result = myDictionary.Contains(new KeyValuePair<int, string>(1, "One"));
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Contains_NullKey_ThrowsArgumentNullException()
+        {
+            var myDictionary = new MyDictionary<object, string>();
+
+            Action action = () => myDictionary.Contains(new KeyValuePair<object, string>(null!, "One"));
+
+            Assert.Throws<ArgumentNullException>(action);
+        }
+
+        [Fact]
+        public void ContainsKey_ExistingKey_ReturnsTrue()
+        {
+            var myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "One");
+
+            bool result = myDictionary.ContainsKey(1);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ContainsKey_NonExistentKey_ReturnsFalse()
+        {
+            var myDictionary = new MyDictionary<int, string>();
+
+            bool result = myDictionary.ContainsKey(1);
+
+            Assert.False(result); 
+        }
+
+        [Fact]
+        public void ContainsKey_NullKey_ThrowsArgumentNullException()
+        {
+            var myDictionary = new MyDictionary<object, string>();
+
+            Action action = () => myDictionary.ContainsKey(null!);
+
+            Assert.Throws<ArgumentNullException>(action);
+        }
+    }
+}
