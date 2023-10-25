b31f53d92be01ee210fd98e45887080340befc1e
NazarPetrakov <nazarpetrakov1@gmail.com>
2023-10-25 18:13:52 +0300

    delete unimportant usings

--- MyCollection.Tests/ContainsTests.cs
+++ MyCollection.Tests/ContainsTests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyCollection.Tests
+﻿namespace MyCollection.Tests
 {
     public class ContainsTests
     {
