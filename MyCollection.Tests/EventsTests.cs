20f8b8923f36497bcf0bbb555ae5eb9f418f7b24
NazarPetrakov <nazarpetrakov1@gmail.com>
2023-10-25 17:47:30 +0300

    add event tests

---
+++ MyCollection.Tests/EventsTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCollection.Tests
+{
+    public class EventsTests
+    {
+        [Fact]
+        public void AddedPairEvent()
+        {
+            bool isEventTriggered = false;
+            var myDictionary = new MyDictionary<int, string>
+            {
+                { 1, "One" },
+            };
+
+            myDictionary.AddedPair += (sender, args) =>
+                isEventTriggered = true;
+            myDictionary.Add(2, "Two");
+
+            Assert.True(isEventTriggered);
+        }
+        [Fact]
+        public void RemovedPairEvent()
+        {
+            bool isEventTriggered = false;
+            var myDictionary = new MyDictionary<int, string>
+            {
+                { 1, "One" },
+            };
+
+            myDictionary.RemovedPair += (sender, args) =>
+                isEventTriggered = true;
+            myDictionary.Remove(1);
+
+            Assert.True(isEventTriggered);
+        }
+        [Fact]
+        public void ChangedValueEvent()
+        {
+            bool isEventTriggered = false;
+            var myDictionary = new MyDictionary<int, string>
+            {
+                { 1, "One" },
+            };
+
+            myDictionary.ChangedValue += (sender, args) =>
+                isEventTriggered = true;
+            myDictionary[1] = "!ONE!";
+
+            Assert.True(isEventTriggered);
+        }
+        [Fact]
+        public void ClearedEvent()
+        {
+            bool isEventTriggered = false;
+            var myDictionary = new MyDictionary<int, string>
+            {
+                { 1, "One" },
+            };
+
+            myDictionary.Cleared += (sender, args) =>
+                isEventTriggered = true;
+            myDictionary.Clear();
+
+            Assert.True(isEventTriggered);
+        }
+    }
+}
